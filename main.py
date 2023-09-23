896f578e4937cb659ff00da9051293e1d96d2a2b
Yangseo Choi <yshoi92@gmail.com>
2023-09-23 23:22:36 +0900

    import modificatiion

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-import iris_scatter
+from iris_scatter import iris_scatter
 import sklearn
 from sklearn.datasets import load_iris
 import matplotlib.pyplot as plt
